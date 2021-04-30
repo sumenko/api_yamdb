1e264b438a4c565377642eeb7f0850b9ebbcca47
Vladimir Sumenko <3760097@gmail.com>
2021-04-30 09:53:05 +0300

    replace OneUserViewset with @action method

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import OneUserViewSet, UsersViewset
+from .views import UsersViewset  # OneUserViewSet
 
 router = DefaultRouter()
 router.register('', UsersViewset, basename='users')
 
 urlpatterns = [
-    path('v1/users/me/', OneUserViewSet.as_view()),
     path('v1/users/', include(router.urls)),
 ]
